ffbda97e8029dec9bef7f95ecc8e5d9d05390241
Felipe Martinez <felipe@pipe01.net>
2026-08-27 21:45:27 +0200

    Hit testing works

--- src/main/java/net/pipe01/cclidar/blocks/LidarBlockEntity.java
+++ src/main/java/net/pipe01/cclidar/blocks/LidarBlockEntity.java
@@ -1,12 +1,41 @@
 package net.pipe01.cclidar.blocks;
 
+import dev.ryanhcode.sable.companion.SableCompanion;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Position;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
 import net.pipe01.cclidar.CCLIDAR;
+import org.joml.Vector3dc;
 
 public class LidarBlockEntity extends BlockEntity {
     public LidarBlockEntity(BlockPos pos, BlockState blockState) {
         super(CCLIDAR.LIDAR_BLOCK_ENTITY.get(), pos, blockState);
+    }
+
+    public Double getHit() {
+        var level = getLevel();
+
+        if (level != null) {
+            var hit = level.clip(new ClipContext(
+                    worldPosition.getCenter(),
+                    worldPosition.getCenter().add(Vec3.atLowerCornerOf(getBlockState().getValue(LidarBlock.FACING).getNormal()).multiply(3, 3, 3)),
+                    ClipContext.Block.VISUAL,
+                    ClipContext.Fluid.ANY,
+                    CollisionContext.empty()
+            ));
+
+            if (hit.getType() == HitResult.Type.BLOCK) {
+                return SableCompanion.INSTANCE.rectilinearDistanceWithSubLevels(level, worldPosition.getCenter(), hit.getLocation());
+            }
+            return null;
+        }
+
+        return null;
     }
 }
--- src/main/java/net/pipe01/cclidar/blocks/LidarPeripheral.java
+++ src/main/java/net/pipe01/cclidar/blocks/LidarPeripheral.java
@@ -3,6 +3,7 @@ package net.pipe01.cclidar.blocks;
 import dan200.computercraft.api.lua.LuaFunction;
 import dan200.computercraft.api.peripheral.GenericPeripheral;
 import dan200.computercraft.api.peripheral.PeripheralType;
+import net.minecraft.world.phys.Vec3;
 
 public class LidarPeripheral implements GenericPeripheral {
     @Override
@@ -11,7 +12,7 @@ public class LidarPeripheral implements GenericPeripheral {
     }
 
     @LuaFunction(mainThread = true)
-    public int test(LidarBlockEntity lidarBlockEntity) {
-        return 42;
+    public Double test(LidarBlockEntity lidarBlockEntity) {
+        return lidarBlockEntity.getHit();
     }
 }

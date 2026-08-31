15d8e1dc7a035381f51780997681e562fbf875e3
Felipe Martinez <felipe@pipe01.net>
2026-08-31 02:52:07 +0200

    Full wrench rotation support with

--- src/main/java/net/pipe01/cclidar/mixins/create/LidarBlockMixin.java
+++ src/main/java/net/pipe01/cclidar/mixins/create/LidarBlockMixin.java
@@ -1,9 +1,38 @@
 package net.pipe01.cclidar.mixins.create;
 
 import com.simibubi.create.content.equipment.wrench.IWrenchable;
+import net.minecraft.core.Direction;
+import net.minecraft.core.FrontAndTop;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 import net.pipe01.cclidar.blocks.LidarBlock;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(LidarBlock.class)
 public abstract class LidarBlockMixin implements IWrenchable {
+    @Override
+    public BlockState getRotatedBlockState(BlockState originalState, Direction targetedFace) {
+        FrontAndTop orientation = originalState.getValue(LidarBlock.ORIENTATION);
+
+        Direction.Axis wasAxis = orientation.front().getAxis();
+        Direction.Axis targetAxis = targetedFace.getAxis();
+
+        FrontAndTop newOrientation;
+        if (wasAxis.isVertical() && targetAxis.isVertical()) {
+            newOrientation = FrontAndTop.fromFrontAndTop(orientation.front(), orientation.top().getClockWise());
+        } else {
+            Direction newFront = Direction.getNearest(Vec3.atLowerCornerOf(orientation.front().getNormal().cross(targetedFace.getNormal())));
+            Direction newUp;
+
+            if (!wasAxis.isVertical() && !targetAxis.isVertical())
+                newUp = orientation.front();
+            else
+                newUp = Direction.UP;
+
+            newOrientation = FrontAndTop.fromFrontAndTop(newFront, newUp);
+        }
+
+        //noinspection ConstantValue
+        return originalState.setValue(LidarBlock.ORIENTATION, newOrientation == null ? orientation : newOrientation);
+    }
 }
